b5c64d9abc5d3d446460633daec48dcae5c71a66
Hindavi <hindavishinde24@gmail.com>
2025-10-09 21:15:46 +0530

    14 Sept 2025

---
+++ src/ex_06_Java_Basics/Lab023_Non_Primitive_Literal.java
@@ -0,0 +1,9 @@
+package ex_06_Java_Basics;
+
+public class Lab023_Non_Primitive_Literal {
+    public static void main(String[] args) {
+        String name = "Hindavi"; // String is bunch of char.
+        int[] arrays_of_items = new int[10];
+
+    }
+}
