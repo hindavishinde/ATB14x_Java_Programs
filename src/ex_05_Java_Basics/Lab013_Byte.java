759dc1b5403f74c21d60592b08ecf1679b16f8c3
Hindavi <hindavishinde24@gmail.com>
2025-10-09 20:32:52 +0530

    14 Sept 2025

---
+++ src/ex_05_Java_Basics/Lab013_Byte.java
@@ -0,0 +1,12 @@
+package ex_05_Java_Basics;
+
+public class Lab013_Byte {
+    public static void main(String[] args) {
+            byte age = 10;
+
+//        String s = "Pramod";
+//        // p,r,a,m,o,d -> char - Primitive
+//
+        }
+    }
+
