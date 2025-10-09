62255b1b5a6620dbd0a2981c65c2f8571d302125
Hindavi <hindavishinde24@gmail.com>
2025-10-09 15:22:10 +0530

    13 Sept 2025 Multi-Comments

---
+++ src/ex_03_Java_Basics/Lab003_Multi_Comments.java
@@ -0,0 +1,19 @@
+package ex_03_Java_Basics;
+
+public class Lab003_Multi_Comments {
+    /**
+     * Author - Hindavi
+     * Course :  ATB11x
+     * This is a documentation comment
+     */
+
+    public static void main(String[] args) {
+        // Method implementation
+
+        //        System.out.println("1");
+        //        System.out.println("2");
+
+    }
+
+
+}
