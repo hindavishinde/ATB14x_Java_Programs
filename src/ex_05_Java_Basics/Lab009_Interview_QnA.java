ec2ba839b0bdf244e0f34c0719c541273f4ca141
Hindavi <hindavishinde24@gmail.com>
2025-10-09 20:15:03 +0530

    13 Sept 2025

---
+++ src/ex_05_Java_Basics/Lab009_Interview_QnA.java
@@ -0,0 +1,9 @@
+package ex_05_Java_Basics;
+
+public class Lab009_Interview_QnA {
+        public static void main(String[] args) {
+            int _2 = 123;
+            System.out.println(_2);
+
+        }
+    }
