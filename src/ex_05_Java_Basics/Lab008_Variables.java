42b49e87ba95fa2eb45baa5d1f23db0b6cb37075
Hindavi <hindavishinde24@gmail.com>
2025-10-09 20:10:24 +0530

    13 Sept 2025

---
+++ src/ex_05_Java_Basics/Lab008_Variables.java
@@ -0,0 +1,11 @@
+package ex_05_Java_Basics;
+
+public class Lab008_Variables {
+    public static void main(String[] args) {
+        // byte age = 128; // Out of range.
+        int age = 128;
+        age = age+1;
+        age = 76;
+        System.out.println(age);
+    }
+}
