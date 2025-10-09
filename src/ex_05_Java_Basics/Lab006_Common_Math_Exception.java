c3c0fdbbfe3c80d75686d9a192a6672d1462c2e4
Hindavi <hindavishinde24@gmail.com>
2025-10-09 19:55:08 +0530

    13 Sept 2025 Common_Math_Exception

---
+++ src/ex_05_Java_Basics/Lab006_Common_Math_Exception.java
@@ -0,0 +1,8 @@
+package ex_05_Java_Basics;
+
+public class Lab006_Common_Math_Exception {
+    public static void main(String[] args) {
+        System.out.println(10/0);
+        // Exception in thread "main" java.lang.ArithmeticException
+    }
+}
