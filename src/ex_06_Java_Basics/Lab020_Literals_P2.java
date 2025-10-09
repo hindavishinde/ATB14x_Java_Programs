d69a38bcbde2aa67c66ad0c90dd1d14721c075e7
Hindavi <hindavishinde24@gmail.com>
2025-10-09 20:57:16 +0530

    14 Sept 2025

---
+++ src/ex_06_Java_Basics/Lab020_Literals_P2.java
@@ -0,0 +1,9 @@
+package ex_06_Java_Basics;
+
+public class Lab020_Literals_P2 {
+    public static void main(String[] args) {
+        float pi = 3.14f;
+        float pi2 = 3.14F;
+        float x = 10.00f;
+    }
+}
--- src/ex_06_Literals/Lab020_Literals_P2.java
+++
@@ -1,5 +0,0 @@
-package ex_06_Literals;
-
-public class Lab020_Literals_P2 {
-
-}
