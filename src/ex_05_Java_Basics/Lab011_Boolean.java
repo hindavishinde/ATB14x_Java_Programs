b75ffaa9fede0e448b51f4383ef7a34401bbec7c
Hindavi <hindavishinde24@gmail.com>
2025-10-09 20:26:14 +0530

    13 Sept 2025

---
+++ src/ex_05_Java_Basics/Lab011_Boolean.java
@@ -0,0 +1,10 @@
+package ex_05_Java_Basics;
+
+public class Lab011_Boolean {
+    public static void main(String[] args) {
+        boolean is_pramod_married = true;
+        boolean prampd_has_audi = false;
+        boolean this_is_a_long_name_that_student_asked_from_us_that_is_it_possible = true;
+
+    }
+}
