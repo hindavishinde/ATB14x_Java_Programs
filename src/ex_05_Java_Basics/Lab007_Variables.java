a70d15601854cac35b1633f0111f3b41a45cbe07
Hindavi <hindavishinde24@gmail.com>
2025-10-09 20:07:28 +0530

    13 Sept 2025

---
+++ src/ex_05_Java_Basics/Lab007_Variables.java
@@ -0,0 +1,9 @@
+package ex_05_Java_Basics;
+
+public class Lab007_Variables {
+        public static void main(String[] args) {
+            int a = 10;
+            a = 65+1;
+            System.out.println(a);
+        }
+    }
