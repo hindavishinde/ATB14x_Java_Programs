2575e680b74ebc80f476181a5147ecd8f1a161cb
Hindavi <hindavishinde24@gmail.com>
2025-10-09 20:44:14 +0530

    14 Sept 2025

---
+++ src/ex_06_Literals/Lab019_Literals_P1.java
@@ -0,0 +1,12 @@
+package ex_06_Literals;
+
+public class Lab019_Literals_P1 {
+    public static void main(String[] args) {
+        int local;
+        local = 10;
+        System.out.println(local);
+
+
+
+    }
+}
