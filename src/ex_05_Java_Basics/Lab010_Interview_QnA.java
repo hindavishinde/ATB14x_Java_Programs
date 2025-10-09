16c23111b724067613e86cafcf261ced333fdae6
Hindavi <hindavishinde24@gmail.com>
2025-10-09 20:19:09 +0530

    13 Sept 2025

---
+++ src/ex_05_Java_Basics/Lab010_Interview_QnA.java
@@ -0,0 +1,13 @@
+package ex_05_Java_Basics;
+
+public class Lab010_Interview_QnA {
+        public static void main(String[] args) {
+//        int enum = 10;
+            int Enum = 10;
+//        System.out.println(enum);
+            //        int hindavi shinde = 89;
+            int hindavi$shinde = 90;
+            int Lab020_Interview_QnA2 = 123;
+
+        }
+    }
