b93163b0729d57c0bccc1ed588e6658b3c88be03
Hindavi <hindavishinde24@gmail.com>
2025-11-11 23:34:28 +0530

    21 Sep

---
+++ src/ex_11_If_Condition/Lab120_Even_Odd.java
@@ -0,0 +1,12 @@
+package ex_11_If_Condition;
+
+public class Lab120_Even_Odd {
+    public static void main(String[] args) {
+        int a = 20;
+        if (a % 2 == 0) {
+            System.out.println("Even");
+        } else {
+            System.out.println("Odd");
+        }
+    }
+}
