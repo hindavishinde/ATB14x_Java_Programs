249fdba9f625e739ba30459396c585cd66765fcf
Hindavi <hindavishinde24@gmail.com>
2025-11-11 23:29:37 +0530

    21 Sep

---
+++ src/ex_11_If_Condition/Lab45_If_Cond.java
@@ -0,0 +1,13 @@
+package ex_11_If_Condition;
+
+public class Lab45_If_Cond {
+    public static void main(String[] args) {
+        int a = 15;
+        if (a == 4) { // since a != 4 so Bye is o/p
+            System.out.println("Hello");
+        } else {
+            System.out.println("Bye");
+        }
+
+    }
+}
