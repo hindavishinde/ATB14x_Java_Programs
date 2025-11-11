8e63a3486348641d953b9d2e25aef4163b4dfa65
Hindavi <hindavishinde24@gmail.com>
2025-11-11 23:37:45 +0530

    21 Sep

---
+++ src/ex_11_If_Condition/Lab49_Maximum_Two_No.java
@@ -0,0 +1,12 @@
+package ex_11_If_Condition;
+
+public class Lab49_Maximum_Two_No {
+    public static void main(String[] args) {
+        int a = 25, b = 45;
+        if (a > b) {
+            System.out.println(a + " is Max");
+        } else {
+            System.out.println(b + " is Max");
+        }
+    }
+}
