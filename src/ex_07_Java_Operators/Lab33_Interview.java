4db55c6b37a052325e2c2638bc77822dac2fc65d
Hindavi <hindavishinde24@gmail.com>
2025-11-11 21:11:09 +0530

    20 Sep

---
+++ src/ex_07_Java_Operators/Lab33_Interview.java
@@ -0,0 +1,9 @@
+package ex_07_Java_Operators;
+
+public class Lab33_Interview {
+    public static void main(String[] args) {
+        int a = 10;
+        boolean b = (10 == 11);
+        System.out.println(b);
+    }
+}
