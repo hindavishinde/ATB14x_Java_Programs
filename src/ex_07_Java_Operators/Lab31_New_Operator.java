8c4e981447b13fc9234a8c9fa87eb468d17f0d37
Hindavi <hindavishinde24@gmail.com>
2025-11-11 21:08:16 +0530

    20 Sep

---
+++ src/ex_07_Java_Operators/Lab31_New_Operator.java
@@ -0,0 +1,10 @@
+package ex_07_Java_Operators;
+
+public class Lab31_New_Operator {
+    public static void main(String[] args) {
+/*        New Operator
+                Non Primitive Variables*/
+        String name = new String("Hindavi");
+//                Learn in OOPS
+    }
+}
