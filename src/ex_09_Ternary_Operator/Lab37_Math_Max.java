49dbcd5b4859673262db320aab0925730b342650
Hindavi <hindavishinde24@gmail.com>
2025-11-11 22:06:10 +0530

    21 Sep

---
+++ src/ex_09_Ternary_Operator/Lab37_Math_Max.java
@@ -0,0 +1,13 @@
+package ex_09_Ternary_Operator;
+
+public class Lab37_Math_Max {
+    public static void main(String[] args) {
+        int a = 60, b = 90;
+        System.out.println(Math.max (a,b));
+//Max no. between 2 nos
+        int max = a > b ? a : b;
+        System.out.println(max);
+
+    }
+}
+
