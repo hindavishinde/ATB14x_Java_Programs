de5f3629b813eba6902ada30d0f3a694d4c00636
Hindavi <hindavishinde24@gmail.com>
2025-11-11 21:12:14 +0530

    20 Sep

---
+++ src/ex_07_Java_Operators/Lab34_Interview.java
@@ -0,0 +1,9 @@
+package ex_07_Java_Operators;
+
+public class Lab34_Interview {
+    public static void main(String[] args) {
+        long l = 300l;
+        String name = "Hindavi";
+//        Bytes, Bits = 8 Bytes, 64 Bits
+    }
+}
