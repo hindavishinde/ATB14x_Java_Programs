40451bfede166d101cb41aaa6f8a2360293947cd
Hindavi <hindavishinde24@gmail.com>
2025-11-11 20:33:19 +0530

    20 Sep

---
+++ src/ex_07_Java_Operators/Lab28_Arithmetic.java
@@ -0,0 +1,9 @@
+package ex_07_Java_Operators;
+
+public class Lab28_Arithmetic {
+    public static void main(String[] args) {
+        System.out.println(!(10>20));
+       /* ! - Not
+                o/p - True*/
+    }
+}
