a0e27303bf861a4e7fdc631d2b22a96394dea962
Hindavi <hindavishinde24@gmail.com>
2025-11-11 20:21:25 +0530

    20 Sep

---
+++ src/ex_07_Java_Operators/Lab26_Bodmas.java
@@ -0,0 +1,13 @@
+package ex_07_Java_Operators;
+
+public class Lab26_Bodmas {
+    public static void main(String[] args) {
+        System.out.println((3*6/5)+6-5);
+/*        3 * 6 = 18;
+        18 / 5 = 3.6
+        3.6 + 6 = 9.6
+        9.6 - 5 = 4.6*/
+    }
+}
+
+
