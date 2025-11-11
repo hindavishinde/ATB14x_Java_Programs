007f881d9a3275f431652b0774b22ae0a2cb4b50
Hindavi <hindavishinde24@gmail.com>
2025-11-11 22:09:05 +0530

    21 Sep

---
+++ src/ex_09_Ternary_Operator/Lab41_Max_Nos.java
@@ -0,0 +1,24 @@
+package ex_09_Ternary_Operator;
+
+public class Lab41_Max_Nos {
+    public static void main(String[] args) {
+        int n1 = 2; int n2 = 9;  int n3 = -11;
+/*        LBF (Logic Building Formula)
+
+                Step 1 - i/p & o/p data types
+                i/p - int datatype
+                o/p - max no. int data type or message max number then string data type
+
+                Step 2 - Rough logic
+            1. n1 > n2 & n1 > n3 = n1
+               2.  2 > n3 & n2 > n1 = n2
+                 3.  else n3
+
+                Step 3 Dry Run*/
+        int max = (n1 > n2) ? (n1 > n3 ? n1 : n3) : (n2 > n3 ? n2 : n3);
+/*      n1 > n2 then 1st expression (n1 > n3 ? n1 : n3) runs True - n1 ; False - n3
+            n2 > n1 then 2nd exp (n2 > n3 ? n2 : n3) runs True - n2 ; False - n3 */
+        System.out.printf("max is %d", max); // since String o/p starting so int comes under string datatype
+        /*        %d for int datatype ; f - formating*/
+    }
+}
