a55f8115f3b40151a1b512b577463891fb526520
Hindavi <hindavishinde24@gmail.com>
2025-11-12 11:28:55 +0530

    28 Sep

---
+++ src/ex_14_WhileLoop/Lab77_Decrement.java
@@ -0,0 +1,11 @@
+package ex_14_WhileLoop;
+
+public class Lab77_Decrement {
+    public static void main(String[] args) {
+        int i1 = 10;
+        while (i1 >= 5) { // 5 runs 10 to 5 as >=
+            System.out.println(i1); // o/p = 10 9 8 7 6 5
+            i1--;
+        }
+    }
+}
