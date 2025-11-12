db809d8055107e3daa468bda2afceec1ba484676
Hindavi <hindavishinde24@gmail.com>
2025-11-12 11:33:23 +0530

    28 Sep

---
+++ src/ex_15_DoWhileLoop/Lab81_DoWhile_Condition.java
@@ -0,0 +1,17 @@
+package ex_15_DoWhileLoop;
+
+public class Lab81_DoWhile_Condition {
+    public static void main(String[] args) {
+        int a = 0;
+        do {
+            System.out.println(a);
+        } while (a < 0);
+        a++; // print 0 as o/p since print present above condition then stops as condition fail
+
+
+        while (a < 0) {
+            System.out.println(a);
+            a++; // no output as condition fail
+        }
+    }
+}
